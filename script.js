d5843956920bce6765f0f59c0b6f9e9407e9504e
João Pedro Garcia <eujoaopedrog@gmail.com>
2023-10-09 22:08:21 -0300

    Project MP3

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+let musicas = [
+    { titulo: 'Casinha (Ao Vivo)', artista: 'Armandinho', src: 'music/Armandinho - Casinha (Ao Vivo).mp3', img: 'img/Praia.jpg' },
+    { titulo: 'Colgate', artista: 'Brandão085', src: 'music/Brandão085 - Colgate.mp3', img: 'img/HipHop.jpg' },
+    { titulo: 'Fendi', artista: 'Brandão085', src: 'music/Brandão085 - Fendi.mp3', img: 'img/HipHop.jpg' }
+
+];
+
 let musica = document.querySelector('audio');
 
-let duracaoMusica = document.querySelector('.end');
+let indexMusica = 0;
 
-duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+let duracaoMusica = document.querySelector('.end');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.description h2');
+let nomeArtista = document.querySelector('.description i');
+
+renderizarMusica(indexMusica);
 
 // Eventos
 document.querySelector('.buttom-play').addEventListener('click', tocarMusica);
@@ -11,33 +23,66 @@ document.querySelector('.buttom-pause').addEventListener('click', pausarMusica);
 
 musica.addEventListener('timeupdate', autalizarBarra);
 
+musica.addEventListener('loadeddata', duration);
+
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2){
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+});
 
 // Funções
-function tocarMusica(){
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
+function tocarMusica() {
     musica.play();
     document.querySelector('.buttom-pause').style.display = 'block';
     document.querySelector('.buttom-play').style.display = 'none';
 }
 
-function pausarMusica(){
+function pausarMusica() {
     musica.pause();
     document.querySelector('.buttom-pause').style.display = 'none';
     document.querySelector('.buttom-play').style.display = 'block';
 }
 
-function autalizarBarra(){
+function autalizarBarra() {
     let barra = document.querySelector('progress');
     barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
     let tempoDecorrido = document.querySelector('.start');
     tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
 }
 
-function segundosParaMinutos(segundos){
+function segundosParaMinutos(segundos) {
     let campoMinutos = Math.floor(segundos / 60);
     let campoSegundos = segundos % 60;
-    if (campoSegundos < 10){
+    if (campoSegundos < 10) {
         campoSegundos = '0' + campoSegundos;
     }
 
-    return  campoMinutos + ':' + campoSegundos;
+    return campoMinutos + ':' + campoSegundos;
 }
+
+function duration() {
+    let duracaoMusica = document.querySelector('.end');
+    duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+}
+
